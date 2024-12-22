8367a267d5bfdab6acf8aec01acf25d63561cf7e
Jordan Cheong <jordancheong520@gmail.com>
2024-12-22 13:56:12 +0800

    add relationships for entities
    (cherry picked from commit b0252b128b3b8b9d9f312b108a6578d07654a0ed)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  has_many :section_item
+  has_many :sections, through: :section_item
+  has_many :item_modifier_group
+  has_many :modifier_group, through: :item_modifier_group
+  has_many :modifier, class_name: "Modifier", foreign_key: "item_id"
 end
