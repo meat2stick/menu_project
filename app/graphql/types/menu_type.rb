98749a9ab6f7cae144c8fb6ae3fa3c474b199c49
Jordan Cheong <jordancheong520@gmail.com>
2024-12-22 14:02:33 +0800

    add entities graphql types
    (cherry picked from commit 68f4de224fd97256911c41e66ca67cdeaf88ffe9)

---
+++ app/graphql/types/menu_type.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Types
+  class MenuType < Types::BaseObject
+    field :id, ID, null: false
+    field :identifier, String
+    field :label, String
+    field :state, String
+    field :start_date, GraphQL::Types::ISO8601Date
+    field :end_date, GraphQL::Types::ISO8601Date
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :sections, [Types::SectionType], null: true
+  end
+
+  # Resolves sections associated with the menu
+  def sections
+    object.sections.to_a
+  end
+end
---
+++ app/graphql/types/section_type.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Types
+  class SectionType < Types::BaseObject
+    field :id, ID, null: false
+    field :identifier, String
+    field :label, String
+    field :description, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :menu_sections, [Types::MenuSectionType], null: true
+    field :items, [Types::ItemType], null: true
+  end
+
+  # Resolver for items associated with the section
+  def items
+    object.items
+  end
+end
