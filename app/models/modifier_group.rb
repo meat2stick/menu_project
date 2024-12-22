b0252b128b3b8b9d9f312b108a6578d07654a0ed
Jordan Cheong <jordancheong520@gmail.com>
2024-12-22 13:56:12 +0800

    add relationships for entities

--- app/models/modifier_group.rb
+++ app/models/modifier_group.rb
@@ -1,2 +1,5 @@
 class ModifierGroup < ApplicationRecord
+  has_many :modifier
+  has_many :item_modifier_group
+  has_many :items, through: :item_modifier_group
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,6 @@
 class Section < ApplicationRecord
+  has_many :menu_sections
+  has_many :menus, through: :menu_sections
+  has_many :section_items
+  has_many :items, through: :section_items
 end
