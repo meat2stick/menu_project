4de5ff3a6964d0f58c11a759a430033a08010356
Jordan Cheong <jordancheong520@gmail.com>
2024-12-23 22:56:16 +0800

    add graph queries for all important entities
    (cherry picked from commit 880e48601f7f1841f4b0ac01542c56fd7e516ec6)

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,34 @@
 
 module Types
   class QueryType < Types::BaseObject
-    # Add a field to fetch all menus
     field :menus, [Types::MenuType], null: false, description: "Retrieve all menus"
 
-    # Define the resolver for the menus field
+    field :sections, [Types::SectionType], null: false, description: "Retrieve all sections"
+
+    field :items, [Types::ItemType], null: false, description: "Retrieve all items"
+
+    field :modifierGroups, [Types::ModifierGroupType], null: false, description: "Retrieve all modifier groups"
+
+    field :modifiers, [Types::ModifierType], null: false, description: "Retrieve all modifiers"
+
     def menus
       Menu.all
+    end
+
+    def sections
+      Section.all
+    end
+
+    def items
+      Item.all
+    end
+
+    def modifierGroups
+      ModifierGroup.all
+    end
+
+    def modifiers
+      Modifier.all
     end
   end
 end
