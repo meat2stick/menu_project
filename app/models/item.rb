50b41b91fdef650cd0c3d9f1ec1ed14952cb61c7
Jordan Cheong <jordancheong520@gmail.com>
2025-01-07 23:18:53 +0800

    add display order field

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
   has_many :item_modifier_group
   has_many :modifier_group, through: :item_modifier_group
   has_many :modifier, class_name: "Modifier", foreign_key: "item_id"
+
+  def display_order(section_id)
+    section_item.find_by(section_id: section_id)&.display_order
+  end
 end
